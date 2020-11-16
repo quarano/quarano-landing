3d81f687fce20138e921ab39f12a7afd5756aa22
Frank <deutschmann@monobloc.de>
2020-11-16 12:08:24 +0100

    mobile nav

---
+++ public/assets/js/script.js
@@ -0,0 +1,23 @@
+console.clear();
+
+const navExpand = [].slice.call(document.querySelectorAll('.nav-expand'));
+const backLink = `<li class="nav-item">
+	<a class="nav-link nav-back-link" href="javascript:;">
+		Back
+	</a>
+</li>`;
+
+navExpand.forEach(item => {
+  item.querySelector('.nav-expand-content').insertAdjacentHTML('afterbegin', backLink);
+  item.querySelector('.nav-link').addEventListener('click', () => item.classList.add('active'));
+  item.querySelector('.nav-back-link').addEventListener('click', () => item.classList.remove('active'));
+});
+
+
+// ---------------------------------------
+// not-so-important stuff starts here
+
+const ham = document.getElementById('ham');
+ham.addEventListener('click', function () {
+  document.body.classList.toggle('nav-is-toggled');
+});
